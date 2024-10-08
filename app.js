32485445b1ea9ba5c73af1652ffe4e66d52d9b4a
Samuel Santos <107240729+samuel-santos91@users.noreply.github.com>
2024-10-08 23:24:52 +1100

    implements email and phone number validation

--- app.js
+++ app.js
@@ -13,6 +13,9 @@ const redirectToMainPage = () => {
 
 const registrationForm = document.getElementById("registration-form");
 
+const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+const phoneRegex = /^(04\d{8}|05\d{8}|02\d{8}|03\d{8}|07\d{8}|08\d{8})$/;
+
 registrationForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
@@ -21,6 +24,7 @@ registrationForm.addEventListener("submit", (e) => {
   const phone = document.getElementById("phone").value;
   const dob = document.getElementById("dob").value;
 
+  // saves data into object
   const userData = {
     name: name,
     email: email,
@@ -28,7 +32,39 @@ registrationForm.addEventListener("submit", (e) => {
     dob: dob,
   };
 
-  registrationForm.reset();
+  //email and phone number validation
+  const emailValid = emailRegex.test(email);
+  const phoneValid = phoneRegex.test(phone);
 
-  console.log(userData);
+  document
+    .getElementById("email-error-message")
+    .classList.toggle("hidden", emailValid);
+  document
+    .getElementById("phone-error-message")
+    .classList.toggle("hidden", phoneValid);
+
+  if (!emailValid || !phoneValid) {
+    return;
+  }
+
+  if (calculateAge(dob) < 18) {
+    location.href =
+      "https://www.regionalaustraliabank.com.au/apply-online/open-rab?p=s3";
+  } else {
+    showThankYouPage();
+  }
+
+  registrationForm.reset();
 });
+
+//checks if user is 18 or over
+const calculateAge = (dob) => {
+  const age = (new Date() - new Date(dob).getTime()) / 3.15576e10;
+
+  return age;
+};
+
+const showThankYouPage = () => {
+  document.getElementById("thank-you-page").classList.remove("hidden");
+  document.getElementById("registration-page").classList.add("hidden");
+};
