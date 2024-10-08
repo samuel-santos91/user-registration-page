8e431d2229a32c0f3a1fba3c5e2f7962858c5bf7
Samuel Santos <107240729+samuel-santos91@users.noreply.github.com>
2024-10-08 20:43:39 +1100

    creates start page and functions for redirection on button click

---
+++ app.js
@@ -0,0 +1,7 @@
+const showRegistrationPage = () => {
+  document.getElementById("registration-page").classList.remove("hidden");
+};
+
+const redirectToMainPage = () => {
+  location.href = "https://www.regionalaustraliabank.com.au";
+};
